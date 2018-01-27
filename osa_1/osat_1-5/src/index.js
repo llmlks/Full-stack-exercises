3de45b092891f52172d3e3a3cfbbc00cf2757869
llmlks <lamelkas@gmail.com>
2018-01-27 11:26:58 +0200

    Teht 1.3

--- osa_1/osat_1-5/src/index.js
+++ osa_1/osat_1-5/src/index.js
@@ -30,28 +30,34 @@ const Yhteensa = (props) => {
 }
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
 
-  return (
-    <div>
-        <Otsikko title={kurssi} />
-        <Sisalto 
-            osa1={osa1} teht1={tehtavia1}
-            osa2={osa2} teht2={tehtavia2}
-            osa3={osa3} teht3={tehtavia3} 
-        />
-        <Yhteensa teht1={tehtavia1} teht2={tehtavia2} teht3={tehtavia3} />
-    </div>
-  )
+    return (
+        <div>
+            <Otsikko title={kurssi} />
+            <Sisalto 
+                osa1={osa1.nimi} teht1={osa1.tehtavia}
+                osa2={osa2.nimi} teht2={osa2.tehtavia}
+                osa3={osa3.nimi} teht3={osa3.tehtavia} 
+            />
+            <Yhteensa teht1={osa1.tehtavia} teht2={osa2.tehtavia} teht3={osa3.tehtavia} />
+        </div>
+    )
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 )
